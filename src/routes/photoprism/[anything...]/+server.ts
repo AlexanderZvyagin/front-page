2cafdd1c4566cf856c8e815004fb8e626b578c50
zvyagin <zvyagin.alexander@gmail.com>
2024-10-24 22:58:23 +0100

    Adding photoprism access

---
+++ src/routes/photoprism/[anything...]/+server.ts
@@ -0,0 +1,21 @@
+// https://stackoverflow.com/questions/77785202/how-to-create-catch-all-route-in-sveltekit-api-dir
+
+import { json } from "@sveltejs/kit";
+import type { RequestHandler } from "@sveltejs/kit";
+
+const server = 'http://az.hopto.org:2342'
+const get_path = (event) => `${server}/${event.params.anything}`;
+
+export async function GET(event: RequestHandler): Promise<Response> {
+    return fetch(get_path(event));
+}
+
+export async function POST(event: RequestHandler): Promise<Response> {
+    return fetch(
+        get_path(event),
+        {
+            method  : 'POST',
+            body    : await event.request.json()
+        }
+    );
+}
